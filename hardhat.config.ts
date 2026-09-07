6a672a7cd778c8f3cae8a4bae4a70c086ad61b3e
nbfy <2545068462@qq.com>
2026-09-07 14:16:14 +0800

    feat: add Hardhat CKCchain configuration

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    ckc: {
+      url: process.env.CKC_RPC_URL || "http://127.0.0.1:8545",
+      chainId: Number(process.env.CKC_CHAIN_ID || 0),
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
